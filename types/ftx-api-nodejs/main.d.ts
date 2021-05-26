08f9be373ca9b4bf3170c7ea1eda9400e13f2d46
dmc0125 <domikmic@gmail.com>
2021-05-26 11:29:18 +0200

    Update: Expoosing types from Ftx module

--- types/ftx-api-nodejs/main.d.ts
+++ types/ftx-api-nodejs/main.d.ts
@@ -1,7 +1,156 @@
 export = Ftx;
 
+declare namespace Ftx {
+  export interface FtxOptions {
+    API_KEY: string;
+    SECRET_KEY: string;
+    SUBACCOUNT?: string;
+  }
+
+  export type Timeframe = '15s' | '1m' | '5m' | '15m' | '1h' | '4h' | '1d';
+
+  // METHOD OPTIONS
+
+  export interface SendOrderOptions {
+    price?: number;
+    reduceOnly?: boolean;
+    ioc?: boolean;
+    postOnly?: boolean;
+    clientId?: any;
+  }
+
+  export interface CandlesticksOptions {
+    startTime?: number;
+    endTime?: number;
+    limit?: number;
+  }
+
+  export interface OrderbookOptions {
+    depth: number;
+  }
+
+  // METHOD RESPONSES
+
+  export interface AccountBalancesResponse {
+    asset: string;
+    available: number;
+    inOrder: number;
+  }
+
+  export interface SendOrderResponse {
+    createdAt: string;
+    filledSize: number;
+    future: string;
+    id: number;
+    market: string;
+    price: number;
+    remainingSize: number;
+    side: string;
+    size: number;
+    status: string;
+    type: string;
+    reduceOnly: boolean;
+    ioc: boolean;
+    postOnly: boolean;
+    clientId: any;
+  }
+
+  export interface CandlesticksResponse {
+    volume: number;
+    openTime: number;
+    closeTime: number;
+    o: number;
+    h: number;
+    l: number;
+    c: number;
+  }
+
+  export interface SingleMarketResponse {
+    name: string;
+    enabled: boolean;
+    postOnly: boolean;
+    priceIncrement: number;
+    sizeIncrement: number;
+    minProvideSize: number;
+    last: number;
+    bid: number;
+    ask: number;
+    price: number;
+    type: string;
+    baseCurrency: string;
+    quoteCurrency: string;
+    underlying: any;
+    restricted: boolean;
+    highLeverageFeeExempt: boolean;
+    change1h: number;
+    change24h: number;
+    changeBod: number;
+    quoteVolume24h: number;
+    volumeUsd24h: number;
+  }
+
+  export type Order = [Number, Number];
+
+  export interface OrderbookResponse {
+    bids: Order[];
+    asks: Order[];
+  }
+
+  // STREAMS OPTIONS
+
+  export interface CandlesticksStreamSettings {
+    market: string;
+    timeframe: Timeframe;
+  }
+
+  // STREAM MESSAGES
+
+  export interface StreamMessage {
+    channel: string;
+    market: string;
+  }
+  
+  export interface SubscribeMessage extends StreamMessage {
+    type: 'subscribed';
+  }
+
+
+  export interface OrderbookData {
+    time: number;
+    checksum: number;
+    bids: Order[];
+    asks: Order[];
+    action: string;
+  }
+
+  export interface OrderbookInitMessage extends StreamMessage {
+    type: 'partial';
+    data: OrderbookData;
+  }
+  
+  export interface OrderbookUpdateMessage extends StreamMessage {
+    type: 'update';
+    data: OrderbookData;
+  }
+
+
+  export interface TickerData {
+    bid: number;
+    ask: number;
+    bidSize: number;
+    askSize: number;
+    last: number;
+    time: number;
+  }
+
+  export interface TickerMessage extends StreamMessage {
+    type: 'update';
+    data: TickerData;
+  }
+}
+
 declare class Ftx {
-  constructor(options?: FtxOptions);
+  constructor(options?: Ftx.FtxOptions);
   public API_URL: string;
   public WS_URL: string;
   public API_KEY: string;
@@ -15,25 +164,25 @@ declare class Ftx {
   private _ftxFetch;
 
   get spot(): {
-    accountBalances: () => Promise<AccountBalancesResponse[]>;
+    accountBalances: () => Promise<Ftx.AccountBalancesResponse[]>;
 
-    sendOrder: (market: string, side: string, type: string, size: number, options?: SendOrderOptions) => Promise<SendOrderResponse>;
+    sendOrder: (market: string, side: string, type: string, size: number, options?: Ftx.SendOrderOptions) => Promise<Ftx.SendOrderResponse>;
 
     convert: (fromCoin: string, toCoin: string, size: number) => Promise<any>;
     
-    candlesticks: (marketName: string, timeframe: Timeframe, options?: CandlesticksOptions) => Promise<CandlesticksResponse[]>;
+    candlesticks: (marketName: string, timeframe: Ftx.Timeframe, options?: Ftx.CandlesticksOptions) => Promise<Ftx.CandlesticksResponse[]>;
 
-    singleMarket: (marketName: string) => Promise<SingleMarketResponse>;
+    singleMarket: (marketName: string) => Promise<Ftx.SingleMarketResponse>;
 
-    orderbook: (marketName: string, options?: OrderbookOptions) => Promise<OrderbookResponse>;
+    orderbook: (marketName: string, options?: Ftx.OrderbookOptions) => Promise<Ftx.OrderbookResponse>;
   };
 
   get spotWebsockets(): {
-    orderbook: (markets: string[], callback: (message: SubscribeMessage | OrderbookInitMessage | OrderbookUpdateMessage) => void) => void;
+    orderbook: (markets: string[], callback: (message: Ftx.SubscribeMessage | Ftx.OrderbookInitMessage | Ftx.OrderbookUpdateMessage) => void) => void;
 
-    ticker: (markets: string[], callback: (message: SubscribeMessage | TickerMessage) => void) => void;
+    ticker: (markets: string[], callback: (message: Ftx.SubscribeMessage | Ftx.TickerMessage) => void) => void;
 
-    candlesticks: ({ market, timeframe }: CandlesticksStreamSettings, callback: (message: CandlesticksResponse) => void) => void;
+    candlesticks: ({ market, timeframe }: Ftx.CandlesticksStreamSettings, callback: (message: Ftx.CandlesticksResponse) => void) => void;
   };
 
   private _callCallback
@@ -41,148 +190,4 @@ declare class Ftx {
   private _initWebsocket
   private _sendMessage;
   private _setCallback;
-}
-
-interface FtxOptions {
-  API_KEY: string;
-  SECRET_KEY: string;
-  SUBACCOUNT?: string;
-}
-
-type Timeframe = '15s' | '1m' | '5m' | '15m' | '1h' | '4h' | '1d';
-
-// METHOD OPTIONS
-
-interface SendOrderOptions {
-  price?: number;
-  size?: number;
-  reduceOnly?: boolean;
-  ioc?: boolean;
-  postOnly?: boolean;
-  clientId?: any;
-}
-
-interface CandlesticksOptions {
-  startTime?: number;
-  endTime?: number;
-  limit?: number;
-}
-
-interface OrderbookOptions {
-  depth: number;
-}
-
-interface CandlesticksStreamSettings {
-  market: string;
-  timeframe: Timeframe;
-}
-
-// METHOD RESPONSES
-
-interface AccountBalancesResponse {
-  asset: string;
-  available: number;
-  inOrder: number;
-}
-
-interface SendOrderResponse {
-  createdAt: string;
-  filledSize: number;
-  future: string;
-  id: number;
-  market: string;
-  price: number;
-  remainingSize: number;
-  side: string;
-  size: number;
-  status: string;
-  type: string;
-  reduceOnly: boolean;
-  ioc: boolean;
-  postOnly: boolean;
-  clientId: any;
-}
-
-interface CandlesticksResponse {
-  volume: number;
-  openTime: number;
-  closeTime: number;
-  o: number;
-  h: number;
-  l: number;
-  c: number;
-}
-
-interface SingleMarketResponse {
-  name: string;
-  enabled: boolean;
-  postOnly: boolean;
-  priceIncrement: number;
-  sizeIncrement: number;
-  minProvideSize: number;
-  last: number;
-  bid: number;
-  ask: number;
-  price: number;
-  type: string;
-  baseCurrency: string;
-  quoteCurrency: string;
-  underlying: any;
-  restricted: boolean;
-  highLeverageFeeExempt: boolean;
-  change1h: number;
-  change24h: number;
-  changeBod: number;
-  quoteVolume24h: number;
-  volumeUsd24h: number;
-}
-
-type Order = [Number, Number];
-
-interface OrderbookResponse {
-  bids: Order[];
-  asks: Order[];
-}
-
-// WEBSOCKETS PAYLOAD
-
-interface StreamMessage {
-  channel: string;
-  market: string;
-}
-
-interface SubscribeMessage extends StreamMessage {
-  type: 'subscribed';
-}
-
-interface OrderbookData {
-  time: number;
-  checksum: number;
-  bids: Order[];
-  asks: Order[];
-  action: string;
-}
-
-interface OrderbookInitMessage extends StreamMessage {
-  type: 'partial';
-  data: OrderbookData;
-}
-
-interface OrderbookUpdateMessage extends StreamMessage {
-  type: 'update';
-  data: OrderbookData;
-}
-
-interface TickerData {
-  bid: number;
-  ask: number;
-  bidSize: number;
-  askSize: number;
-  last: number;
-  time: number;
-}
-
-interface TickerMessage extends StreamMessage {
-  type: 'update';
-  data: TickerData;
 }
